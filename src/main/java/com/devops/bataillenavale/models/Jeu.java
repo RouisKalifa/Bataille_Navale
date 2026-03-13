818891fccdc0850f5f6959b72fe187d59d928b56
Kalifa <kalifarouis@leschassagnes.net>
2026-03-13 09:43:29 +0100

    premier commit test

---
+++ src/main/java/com/devops/bataillenavale/models/Jeu.java
@@ -0,0 +1,22 @@
+package com.devops.bataillenavale.models;
+
+public class Jeu {
+
+    public Joueur joueur1;
+    public Joueur joueur2;
+    public Joueur joueurActif;
+    public boolean partieTerminee;
+}
+
+
+public void demarrerPartie() {}
+
+public ResultatTir tirer(int x,int y){}
+
+public void changerJoueur(){}
+
+public void verifierVictoire(){}
+
+public void getJoueurActif(){}
+
+public void getAdversaire(){}
